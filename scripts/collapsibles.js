adc3bb4219c21229a20a807d49b7a50a69f640a9
Ray <raytspain@gmail.com>
2019-04-22 15:34:36 +0800

    Replaced collapsibles with accordions which now works.

--- scripts/collapsibles.js
+++ scripts/collapsibles.js
@@ -1,14 +1,14 @@
-var coll = document.getElementsByClassName("collapsible");
+var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-coll[i].addEventListener("click", function() {
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
   this.classList.toggle("active");
-  var content = this.nextElementSibling;
-  if (content.style.display === "block") {
-    content.style.display = "none";
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
   } else {
-    content.style.display = "block";
+    panel.style.display = "block";
   }
 });
 }
