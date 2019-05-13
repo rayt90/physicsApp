d719d13e70684e740baf3b1a469a5eabda213139
Ray <raytspain@gmail.com>
2019-05-13 20:32:02 +0800

    Implemented more math to javascript file.

--- scripts/equations.js
+++ scripts/equations.js
@@ -35,3 +35,26 @@ function Work() {
   var w = work.toFixed(2);//showing result with 2 decimal places
   document.getElementById("work").innerHTML = w;
 }
+
+//--------Kinetic Energy Equation---------
+function Kinetic() {
+  'use strict';
+  var m = document.getElementById("KineticM").value;
+  var v = document.getElementById("KineticV").value;
+  var k = (m*Math.pow(v,2))/2;
+  document.getElementById("Kinetic").innerHTML = k;
+}
+
+//---------1st law of motion---------------
+function FirstEq(u, a, t) {
+  'use strict';
+  var u = document.getElementById("firstU").value;
+  var a = document.getElementById("firstA").value;
+  var t = document.getElementById("firstT").value;
+  var v = parseInt(u) + parseInt(a)*parseInt(t);
+  document.getElementById("firsteq").innerHTML = v;
+  console.log(u);
+  console.log(a);
+  console.log(t);
+  console.log(v);
+}
